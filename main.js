f4d39e23a49c9d25248e5a7850614b845200ecae
ansumana <ansumana.d@yahoocom>
2020-10-28 14:45:55 +0100

    changed some styles on mobile view

--- main.js
+++ main.js
@@ -35,23 +35,3 @@ $(document).ready(function() {
 		}
 	});
 });
-
-//greeting text
-$(document).ready(function() {
-	// typing animation
-	(function($) {
-		$.fn.writeText = function(content) {
-			var contentArray = content.split(''),
-				current = 0,
-				elem = this;
-			setInterval(function() {
-				if (current < contentArray.length) {
-					elem.text(elem.text() + contentArray[current++]);
-				}
-			}, 80);
-		};
-	})(jQuery);
-
-	// input text for typing animation
-	$('#holder').writeText('WEB DESIGNER + FRONT-END DEVELOPER');
-});
